6fc77e18ca4dc72bdb18e2f616c132f6ddfe9608
Gideon Dresdner <gideond@gmail.com>
2015-01-11 10:45:26 +0200

    getting started with gulp

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    notify = require('gulp-notify'),
+    cache = require('gulp-cache'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
+
+gulp.task('js', function() {
+    return  gulp.src('./src/js/**/*.js')
+                .pipe(jshint())
+                .pipe(jshint.reporter('default'))
+                .pipe(concat('main.js'))
+                .pipe(gulp.dest('dist/assets/js'))
+                .pipe(rename({suffice: 'min'}))
+                .pipe(uglify())
+                .pipe(gulp.dest('dist/assets/js'))
+                .pipe(notify({ message: 'Done us JavaScript' }));
+});
+
+gulp.task('clean', function(cb) {
+    del(['dist/js'], cb)
+});
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('js');
+});
